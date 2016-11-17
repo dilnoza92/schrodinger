720145d164f318924f386d5524eca4560565b342
Dilnoza Amirkulova <dilnoza92@gmail.com>
2016-11-17 17:27:58 -0500

    terminal

--- schrodinger/schrodinger.py
+++ schrodinger/schrodinger.py
@@ -1,6 +1,7 @@
  # -*- coding: utf-8 -*-
 import numpy as np
 import math
+import argparse
 import matplotlib
 matplotlib.use('Agg')
 
@@ -12,13 +13,30 @@ from scipy.special import legendre
 #if one wants to use fourier the variables should be:  fourier=True and legendre=False
 #if one wants to use legendre it variables  be:  fourier=False and legendre=True
 
-pe=1#potential energy
+pe=1                 #potential energy
 fourier_check=True               #tell to use fourier series for the basis set 
 legendre_check=False             #tells to use legendre polynomials if it is true
-constant=4                    #constant needed for hamiltonian operator
-size_basis_set=150                      #size of the basis set
-positions=np.arange(-1,1, 0.01)        #domain on which the wave function is defined
-period=np.max(positions)-np.min(positions)                               #period for fourier serier
+constant=1                   #constant needed for hamiltonian operator
+size_basis_set=20       #size of the basis set
+
+parser=argparse.ArgumentParser()                  #parses command line inputs                                                                                                              
+parser.add_argument('--fourier', nargs="?")       #adds a flag for fourier has to be a boolean                  
+parser.add_argument('--legendre', nargs="?")      #adds a flag for legendre has to be a boolean
+parser.add_argument('--constant', nargs="?")          #adds a flag for constant
+parser.add_argument('--potential_energy', nargs="?")  #adds a flag for potential energy
+parser.add_argument('--input_file', nargs="?")        #adds a flag for input file 
+parser.add_argument('--basis_set_size', nargs="?")    #adds a flag for basis set size
+parser.add_argument('schrodinger/schrodinger.py', nargs='?')#makes sure that parser doesn't give error when it reads the source code                                     
+arguments=parser.parse_args()                 #arguments given in the command line                                                                                                        
+print arguments
+if (arguments.potential_energy!=None):
+   pe=float(arguments.potential_energy)                 #potential energy
+   fourier_check=arguments.fourier               #tell to use fourier series for the basis set 
+   legendre_check=arguments.legendre             #tells to use legendre polynomials if it is true
+   constant=float(arguments.constant)                   #constant needed for hamiltonian operator
+   size_basis_set=int(arguments.basis_set_size)       #size of the basis set
+positions=np.arange(-1,1, 0.01)               #domain on which the wave function is defined
+period=np.max(positions)-np.min(positions)    #period for fourier serier
 y_psi=np.array([(np.sin(2*np.pi*t)+t**3) for t in positions])  #Wave function
 
 def cn_fourier(n):
@@ -46,7 +64,7 @@ def fourier(x, Nh):
    return f.sum()
 
 
-y_fourier = np.array([fourier(t,size_basis_set).real for t in positions])    #fourier 
+y_fourier = np.array([fourier(t,size_basis_set).real for t in positions])    #fourier approximation of the wave function
 
 
 def cn_legendre(x,y):
@@ -66,7 +84,7 @@ for i in range(len(legendre_coeff)):
    if (abs(legendre_coeff[i])<1*10**(-10)):
       legendre_coeff[i]=0
 #print tuple(map(tuple, legendre_coeff))
-fourier_coeff=np.array([cn_fourier(size_basis_set) for i in np.arange(0.5,size_basis_set+05)])         #vector of fourier coefficients
+fourier_coeff=np.array([cn_fourier(size_basis_set) for i in np.arange(0.5,size_basis_set+0.5)])         #vector of fourier coefficients
 
 y_legendre=np.polynomial.legendre.legval(positions,legendre_coeff)     #legendre polynomial of the wave function
 
@@ -83,7 +101,7 @@ def fourier_deriv(x,Nh):
    sum=f.sum()
    return sum
 def final_fourier_deriv():
-   y_fourie_dev = np.array([fourier_deriv(x,size_basis_set).real for x in np.arange(0.5,size_basis_set+05)])         #fourier     
+   y_fourie_dev = np.array([fourier_deriv(x,size_basis_set).real for x in np.arange(0.5,size_basis_set+0.5)])         #fourier     
    return y_fourie_dev
 
 def laplacian(leg_check,array_coeff):
@@ -137,47 +155,73 @@ pe_data=potential_force_data(filename)   #saves the data in the pe_data array
 
 
 def hamiltonian_legendre(coef):
-      hamilton=laplacian(legendre_check,coef)       
+   
+   """  a function that takes in the basis set coefficients for legendre  and gives the hamiltonian
+   Variables:
+   
+      coef-fourier basis set coeffiecients
+
+   Returns:
+    
+      hamiltonian_last- an array of length of size the basis set that contains hamiltonian 
+   """   
+   hamilton=laplacian(legendre_check,coef)  #the laplacian of the legendre polynomial       
       #hamilton=np.ndarray.tolist(hamilton)  
-      hamilton.append(0)
-      hamilton.append(0)
-      hamilton=np.asarray(hamilton)
-      hamiltonian=np.zeros((len(hamilton)))
-      for i in np.arange(len(hamilton)):
-         hamiltonian[i]=pe*coef[i]+hamilton[i]*(-constant)
-      hamiltonian_last=np.zeros((len(hamilton)))
-      for i in np.arange(len(hamiltonian)):
-         hamiltonian_last[i]=hamiltonian[i]*coef[i]
-      return hamiltonian_last
+   hamilton.append(0)                       #due to the removal of two points from the basis for laplacian two more points need to be appended to make the array have the size of the basis set
+   hamilton.append(0)                       #the second addition 
+   hamilton=np.asarray(hamilton)            #converts the list to an array
+   hamiltonian=np.zeros((len(hamilton)))    #initializes an array of the size of the basis set to keep the hamiltonian before it is multiplied by the basis set coefficients
+   for i in np.arange(len(hamilton)):
+      hamiltonian[i]=pe*coef[i]+hamilton[i]*(-constant) #computes the laplacian plus the potential energy term of basis set coefficient
+   hamiltonian_last=np.zeros((len(hamilton)))           #initializes an array to keep the final solution
+   for i in np.arange(len(hamiltonian)):
+      hamiltonian_last[i]=hamiltonian[i]*coef[i]        #computes the hamiltonian on the basis set functions
+   print len(hamiltonian_last)
+   return hamiltonian_last                              #the final hamiltonian
+
 
 
 def hamiltonian_fourier( coef):
-      hamilton=final_fourier_deriv()
-      print len(hamilton), len(coef)
-      ##hamilton=np.ndarray.tolist(hamilton)
-      #hamilton.append(0)
-      #hamilton.append(0)
-      #hamilton=np.asarray(hamilton)
-      
-      #hamilton[len(hamilton)+1]=0
-      #hamilton[len(hamilton)+1]=0
-      hamiltonian=np.zeros((len(hamilton)))
-      #print len(coef),len(hamilton)
-      for i in np.arange(len(hamilton)):
-         hamiltonian[i]=pe*coef[i]+hamilton[i]*(-constant)
-      hamiltonian_last=np.zeros((len(hamilton)))
-      for i in np.arange(len(hamiltonian)):
-         hamiltonian_last[i]=hamiltonian[i]*coef[i]
-      return hamiltonian_last
-#print hamiltonian_fourier(fourier_coeff)
+   """  a function that takes in the basis set coefficients for fourier and gives the hamiltonian
+   Variables:
+   
+      coef-fourier basis set coeffiecients
+
+   Returns:
+    
+      hamiltonian_last- an array of length of size the basis set that contains hamiltonian 
+   """
+   hamilton=final_fourier_deriv()          #laplacian of the fourier 
+ 
+   hamiltonian=np.zeros((len(hamilton)))   #initiation of an array that will keep the hamiltonians
+   for i in np.arange(len(hamilton)):
+      hamiltonian[i]=pe*coef[i]+hamilton[i]*(-constant)    #computes the laplacian plus the potential energy term of basis set coefficient
+   hamiltonian_last=np.zeros((len(hamilton)))              #initializes an array to keep the final solution
+   for i in np.arange(len(hamiltonian)):
+      hamiltonian_last[i]=hamiltonian[i]*coef[i]           #computes the hamiltonian on the basis set functions
+   return hamiltonian_last
+
 def answer(check):
-   a=[]
-   if (check==False):
-      a=hamiltonian_legendre(legendre_coeff)
+   """ takes a boolean for whether to set legendre polynomial true or false and gives the hamiltonians and a 1
+   Variables:
+
+      check-boolean for legendre polynomial
+
+   Returns:
+
+      a,1-hamiltonian matrix and number 1
+   """
+
+   a=[]#saves the hamiltonian coefficients
+   if (check==False):#checks whether to use legendre polynomial or not
+      a=hamiltonian_legendre(legendre_coeff)#get the hamiltonian using legendre polynomials
    else:
-      a=hamiltonian_fourier(fourier_coeff)
+      a=hamiltonian_fourier(fourier_coeff)#gets the hamiltonian using fourier transforms
    return a,1
-answers=answer(legendre_check)  
+answers=answer(legendre_check)  #hamiltonian
+print answers[0], len(answers[0])
 print 'The hamiltonian of is shown below for Legendre={} :\n {}'.format(legendre_check,answers[0])
-#plt.show()
+
+
+
 
--- tests/test_schrodinger.py
+++ tests/test_schrodinger.py
@@ -8,7 +8,7 @@ test_schrodinger
 Tests for `schrodinger` module.
 """
 
-
+import argparse
 import sys
 import unittest
 from contextlib import contextmanager
@@ -28,8 +28,21 @@ class TestSchrodinger(unittest.TestCase):
         pass
 
     def test_000_something(self):
-        pass
+        
 
+        parser1=argparse.ArgumentParser()                  #parses command line inputs                  \
+            
+        parser1.add_argument('--fourier', nargs="?")       #adds a flag for fourier has to be a boolean \
+            
+        parser1.add_argument('--legendre', nargs="?")      #adds a flag for legendre has to be a boolean
+        parser1.add_argument('--constant', nargs="?")          #adds a flag for constant                 
+        parser1.add_argument('--potential_energy', nargs="?")  #adds a flag for potential energy         
+        parser1.add_argument('--input_file', nargs="?")        #adds a flag for input file               
+        parser1.add_argument('--basis_set_size', nargs="?")    #adds a flag for basis set size           
+        parser1.add_argument('schrodinger/schrodinger.py', nargs='?')#makes sure that parser doesn't giv\
+            
+        arguments=parser1.parse_args()     
+        pass
 
     def test_final_deriv(self):
        # schrodinger.legendre_check==False
